e2a764af25a1b76d577e84d00b4a48dd51024b46
data-day-life <mtvatemybrains@gmail.com>
2020-03-26 13:37:30 -0400

    Success: (neo4j db) adding a streamer nodes, then adding their followers as nodes with relationships.  Relationships are limited to 'node "FOLLOWS" streamer' (may need to be reversed or add both directions)

--- app/models/neo4_db.py
+++ app/models/neo4_db.py
@@ -1,165 +1,136 @@
 from app import settings
 import logging
 from app.twitch_client import TwitchStreamer
-from py2neo import Graph, Node
+from py2neo import Graph
 from py2neo.ogm import GraphObject, Property, Label, RelatedTo, RelatedFrom
 
 module_logger = logging.getLogger(__name__+'.py')
 
 # for logging in fxn def outside class def
 # https://docs.python.org/3/howto/logging-cookbook.html#using-logging-in-multiple-modules
-# module_logger = logging.getLogger('spam_application.auxiliary')
-# TODO: END OF DAY GOAL -- ADD FOLLOWERS TO STREAMER; ADD FOLLOWERS AS 'USERS' IN DB
-# inspect.currentframe().f_code.co_name    gets name of outer function (for logging?)
 
-# DEFINITIONS FOR GRAPH DB OBJECTS
-class UserGraphObj(GraphObject):
-    # The primary node label used for Cypher MATCH and MERGE operations
-    __primarylabel__ = 'User'
+graph = Graph(
+    host=settings.NEO4J_HOST,
+    port=settings.NEO4J_PORT,
+    user=settings.NEO4J_USER,
+    password=settings.NEO4J_PASSWORD
+)
+
+
+class BaseModel(GraphObject):
+    """Generic  base class for all py2neo/neo4j graph objects. """
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+
+    @property
+    def all(self):
+        return self.match(graph)
+
+    def save(self):
+        graph.push(self)
+
+# class User(BaseModel):
+#     # The primary property key used for Cypher MATCH and MERGE operations
+#     __primarykey__ = 'twitch_uid'
+#     # The primary node label used for Cypher MATCH and MERGE operations
+#     __primarylabel__ = 'User'
+#
+#     # PROPERTIES
+#     twitch_uid = Property()
+#     display_name = Property()
+#
+#     # RELATIONSHIPS
+#     is_following = RelatedTo('Streamer', 'IS_FOLLOWING')
+#     is_followed_by = RelatedFrom('User', 'IS_FOLLOWED_BY')
+
+
+class User(BaseModel):
     # The primary property key used for Cypher MATCH and MERGE operations
     __primarykey__ = 'twitch_uid'
 
-    # PROPERTIES
-    twitch_uid = Property('twitch_uid')
-    name = Property('name')
-    display_name = Property('display_name')
-    broadcaster_type = Property('broadcaster_type')
-
-    # RELATIONSHIPS
-    is_following = RelatedTo('Streamer', 'IS_FOLLOWING')
-    is_followed_by = RelatedFrom('User', 'IS_FOLLOWED_BY')
-
-    def __init__(self, twitch_uid):
-        self.twitch_uid = twitch_uid
-
-
-class StreamerNode(UserGraphObj):
-    __primarylabel__ = 'Streamer'
-    # PROPERTIES
-    profile_img_url = Property()
     # LABELS
     #   A Label defined on a GraphObject provides an accessor to a label of the underlying central node.
     #   It is exposed as a boolean value, the setting of which allows the label to be toggled on or off.
     streamer = Label('Streamer')
 
-    is_followed_by = RelatedFrom('User', 'IS_FOLLOWED_BY')
-    is_following = RelatedTo('Streamer', 'IS_FOLLOWING')
+    # PROPERTIES
+    twitch_uid = Property()
+    name = Property()
+    display_name = Property()
+    profile_img_url = Property()
+    broadcaster_type = Property()
+    total_followers = Property()
+    total_followings = Property()
+
+    # RELATIONSHIPS
+    # is_followed_by = RelatedFrom('User', 'IS_FOLLOWED_BY')
+    # is_following = RelatedTo('User', 'IS_FOLLOWING')
+    follows = RelatedTo('User', 'FOLLOWS')
 
 
+def create_from_twitch_streamer(some_streamer: TwitchStreamer) -> User:
+    db_streamer = None
+    try:
+        db_streamer = User()
+        db_streamer.streamer = some_streamer.is_streamer
+        db_streamer.twitch_uid = some_streamer.twitch_uid
+        db_streamer.name = some_streamer.name
+        db_streamer.display_name = some_streamer.display_name
+        db_streamer.profile_img_url = some_streamer.profile_img_url
+        db_streamer.broadcaster_type = some_streamer.broadcaster_type
+        db_streamer.total_followers = some_streamer.total_followers
+        db_streamer.save()
+        module_logger.info("Added {} to DB".format(some_streamer.display_name))
 
-class Neo4jDB:
-    def __init__(self, host_addr: str = 'localhost'):
-        # Initialize logger
-        self.logger = logging.getLogger(__name__+'.py')
-        self.logger.info('Creating an instance of "Neo4jDB"')
-        # Establish a connection to neo4j db
-        self.logger.info('Connecting to neo4j DB ...')
-        self.db = Graph(auth=(settings.NEO4J_USER, settings.NEO4J_PASSWORD), host=host_addr)
-        self.logger.info('... Connected to neo4j DB named: "{}"'.format(self.db.name))
+    except Exception as exc:
+        module_logger.error("Failed to add streamer to db:  {}".format(some_streamer.display_name))
+        module_logger.error(exc.message, exc.args)
 
-    def create_streamer_node(self, some_streamer) -> StreamerNode:
-        streamer_node = StreamerNode(some_streamer.twitch_uid)
-        streamer_node.display_name = some_streamer.display_name
-        streamer_node.name = some_streamer.name
-        streamer_node.profile_img_url = some_streamer.prof_img_url
-        streamer_node.streamer = True
-        # Push Changes to DB for New Streamer
-        module_logger.info("Merging {} to DB".format(some_streamer.display_name))
-        self.db.merge(streamer_node)
+    return db_streamer
 
-        return streamer_node
+# TODO: Refactor so that this work with TwitchStreamer or TwitchUser objects (refactor to_from in these classes')
+# TODO: If creatinga "streamer set" from this result, then we do not need to return a dict (only a list/set of followers)
+def add_all_followers(some_streamer: TwitchStreamer, db_streamer: User) -> dict:
+    """
+    Adds all followers to db for a given TwitchAccount object and py2neo ogm object
+    :param some_streamer:
+    :param db_streamer:
+    :return:
+    """
+    foll_list = some_streamer.get_all_follows()
+    to_from = some_streamer.to_from
+    # A dictionary of {'twitch_uid': db_object}; useful for adding follower's followings
+    all_db_folls_dict = {}
 
-    def add_streamer_folls_from_twitch(self, some_streamer: TwitchStreamer, streamer_node: StreamerNode):
-        followers_dict = some_streamer.get_all_follows()
-        module_logger.info('Adding Followers to DB for {} ...'.format(some_streamer.display_name))
-        # Add followers in followers list as nodes
-        for each_hundred_list in followers_dict.values():
-            for each_follower_dict in each_hundred_list:
-                follower_uid = each_follower_dict['from_id']
-                some_follower = UserGraphObj(follower_uid)
-                some_follower.followed_at = each_follower_dict['followed_at']
-                some_follower.display_name = each_follower_dict['from_name']
-                # Add Follower/Following Relationship between Streamer and Follower
-                #TODO: BREAKS HERE: "module 'neo4db' has no attribute 'Streamer'"
-                some_follower.is_following(streamer_node)
-                # Create or Update Follower in DB
-                self.db.merge(some_follower)
-                module_logger('DB: Added {} as a follower of {}'.format(some_follower.display_name, some_streamer.display_name))
-                # Update Streamer Node Relationship in DB
-                self.db.push(streamer_node.is_followed_by(some_follower))
-                module_logger('Added "is following" relationship for {}'.format(some_follower.display_name))
+    for each_hundred in foll_list.keys():
+        for each_fol in foll_list[each_hundred]:
+            try:
+                db_follower = User()
+                db_follower.twitch_uid = each_fol['from_uid']
+                db_follower.display_name = each_fol['from_name']   # breaks on names like '抜け' without unicode encoding
+                db_follower.follows.add(db_streamer, properties={'followed at': each_fol['followed_at']})
+                # Commit to new follower to DB
+                db_follower.save()
+            except Exception as exc:
+                module_logger.error("Failed to add follower to db: {}".format(some_streamer.display_name))
+                module_logger.error(exc.message, exc.args)
 
-    def find_or_add_streamer(self, streamer_name: str):
-        # Lookup streamer_name using twitch_client to fetch twitch_uid
-        try:
-            streamer = TwitchStreamer(str(streamer_name))
-            twitch_api_succ_msg = "find_or_add_streamer() => Twitch API - 'validate_name()' found user: "
-            twitch_api_succ_msg += '"{}"'.format(streamer.display_name)
-            twitch_api_succ_msg += ' (uid: {})'.format(streamer.twitch_uid)
-            self.logger.info(twitch_api_succ_msg)
-        except ValueError:
-            twitch_api_fail_msg = "'find_or_add_streamer()' => Twitch API user NOT found: "
-            twitch_api_fail_msg += '"{}"'.format(str(streamer_name))
-            self.logger.info(twitch_api_fail_msg)
-            return None
+            all_db_folls_dict[db_follower.twitch_uid] = db_follower
 
-        # Search DB for node matching
-        user_node = Node('User', 'Streamer',
-                         twitch_uid=str(streamer.twitch_uid),
-                         name=str(streamer.name),
-                         display_name=str(streamer.display_name),
-                         total_followers=str(streamer.total_followers),
-                         profile_img_url=str(streamer.prof_img_url))
-
-        streamer_node = StreamerNode()
-        streamer_node.twitch_uid = streamer.twitch_uid
-        streamer_node.display_name = streamer.display_name
-        streamer_node.name = streamer.name
-        streamer_node.profile_img_url = streamer.prof_img_url
+    return all_db_folls_dict
 
 
-        #self.db.create(user_node)
-        print(self.db.nodes)
+def get_streamer_set_from_foll_dict(db_folls_dict: dict):
+    """
+    Takes a dictionary of {twitch_uid: db_follower} and creates a set of streamers that all followers follow
 
-        # # Check DB for unique twitch uid
-        # uid_node = Node('User', twitch_id=str(streamer.uid))
-        # self.logger.info('Checking Neo4j DB for matching twitch uid')
-        # #matcher = NodeMatcher(self.db)
-        # matcher = NodeMatcher(self.db).match(uid_node)
-        # print('matcher', matcher)
-        #
-        # # TODO: Use MERGE to create if not exists
-        # # If DB match not found, try less-restrictive
-        # if matcher is None:
-        #     self.logger.info('No Match found, attempting to add user to DB.')
-        #     new_user_node = Node('User',
-        #                          twitch_id=streamer.uid,
-        #                          name=streamer.name,
-        #                          display_name=streamer.display_name,
-        #                          total_followers=streamer.total_followers,
-        #                          profile_img_url=streamer.prof_img_url)
-        #     self.db.create(new_user_node)
-        #     self.logger.info('New user was created in DB')
-        #
-        #     # Add uid, profile img url, name, total_twitch_followers (as reported by Twitch)
-        #
-        #     # Add follower list relationships from streamer.get_follows
-        #
-        # # Node found in DB
-        # else:
-        #     twitch_total_followers = streamer.total_followers
-        #     # Check if node_total_followers field exists in found node
-        #
-        #     # Check if node_total_followers field  is current (equal to twitch_total_followers)
-        #         # Update follower relationships if 'node_total_followers' != twitch_total_followers
-        #
-        #     # Check COUNT of follower relationships in DB
-        #
-        #         # Update follower relationships if COUNT does not match 'node_total_followers'
-        #
-        #         # Update follower relationships if COUNT does not match 'total_twitch_followers'
+    :param db_folls_dict:
+    :return:
+    """
+    all_streamers = {}
 
-
+    for each_foll_id in db_folls_dict.keys():
+        # Get a list of who a follower follows & add it to all_streamers list
         pass
-
-
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from app import neo4_db
-from app.twitch_client import TwitchStreamer
+from app.twitch_client import TwitchStreamer, TwitchAccount, TwitchUser
+from app.models import neo4_db
 import logging
 
 # Create a new logger instance for this application
@@ -9,18 +9,57 @@ logger = logging.getLogger()
 def main():
     # Get info about a streamer from twitch
     tmp_twitch_streamer = TwitchStreamer('stroopC')
-    fols = tmp_twitch_streamer.get_all_follows()
+    db_streamer = neo4_db.create_from_twitch_streamer(tmp_twitch_streamer)
+    added_folls_dict = neo4_db.add_all_followers(tmp_twitch_streamer, db_streamer)
 
-    for each_hundred_list in fols.keys():
-        print(fols[each_hundred_list])
+    # For each follower in added_folls_dict, get a list of who they follow
+    
+    
+    #fols = tmp_twitch_streamer.get_all_follows()
 
+    #for each_hundred_list in fols.keys():
+        #print(fols[each_hundred_list])
+
+
+    def add_to_db(TwitchStreamer):
+        db_streamer = neo4_db.User()
+        db_streamer.streamer = True
+        db_streamer.twitch_uid = TwitchStreamer.twitch_uid
+        db_streamer.name = TwitchStreamer.name
+        db_streamer.display_name = TwitchStreamer.display_name
+        db_streamer.profile_img_url = TwitchStreamer.profile_img_url
+        db_streamer.broadcaster_type = TwitchStreamer.broadcaster_type
+        db_streamer.total_followers = TwitchStreamer.total_followers
+        #db_streamer.save()
+
+        foll_list = TwitchStreamer.get_all_follows()
+        foll_ids = ''
+
+        for each_hundred_list in foll_list.keys():
+            for each_fol in foll_list[each_hundred_list]:
+                db_foll = neo4_db.User()
+                db_foll.twitch_uid = each_fol['from_id']
+                #db_foll.display_name = each_fol['from_name']   # breaks on names like '抜け' without unicode encoding
+                #db_streamer.is_followed_by.add(db_foll,properties={'followed at': each_fol['followed_at']})
+                db_foll.follows.add(db_streamer, properties={'followed at': each_fol['followed_at']})
+                db_foll.save()
+                foll_ids += each_fol['from_id']
+
+        #db_streamer.save()
+
+    add_to_db(tmp_twitch_streamer)
+
+
+
+
+    print("")
     #db = neo4_db.Neo4jDB()
     #db.find_or_add_streamer('stroopc')
     #folls = tmp_twitch_streamer.get_all_follows()
     #streamer_node = db.create_streamer_node(tmp_twitch_streamer)
     #db.add_streamer_folls_from_twitch(tmp_twitch_streamer, streamer_node)
 
-    pass
+
 
 
 def logging_setup():
