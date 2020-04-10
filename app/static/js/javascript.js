46d672a0a9806361db8b1a0efb0112c1e0dce4e3
Tayg0 <36306875+Tayg0@users.noreply.github.com>
2020-04-10 05:54:45 -0400

    Added temporary placeholder functions for filling in data.
    Finished recommended raid cards. Polished styling.

--- app/static/js/javascript.js
+++ app/static/js/javascript.js
@@ -1,3 +1,18 @@
+
+var client_id = "va97w97mn1qzq0nlrjavlifr92lstz"; //Twitch-API Client ID
+
+var result_template = 
+'<a class="result_card" href="https://www.twitch.tv/{NAME}">\
+<img src="{THUMBNAIL}">\
+<div class="gradient"></div>\
+<img class="avatar" src="https://static-cdn.jtvnw.net/user-default-pictures-uv/75305d54-c7cc-40d1-bb9c-91fbe85943c7-profile_image-150x150.png">\
+<p class="stream_username">{NAME}</p>\
+<p class="stream_title">{TITLE}</p>\
+<p class="stream_viewers">•{VIEWERS}</p>\
+<p class="result_label">{INDEX}</p>\
+</a>';
+
+
 function updateNav(){ //Updates Navbar and currently displayed content based on URL hash path.
 
     if(location.hash === ""){location.hash = "#Home"};
@@ -42,9 +57,83 @@ function updateNav(){ //Updates Navbar and currently displayed content based on 
 
 }
 
+
+const options = {
+    responseType: 'json',
+    headers: {'Client-ID': client_id}
+};
+
+function getID(username){
+
+    axios.get('https://api.twitch.tv/helix/users?login=' + encodeURI(username), options).then(response => {
+
+        if (response.data.data.length > 0){
+            
+            //console.log(response.data.data[0].id);
+            getFollows(response.data.data[0].id);
+            console.log(response.data.data);
+            
+        }else{
+
+            console.log("INVALID USERNAME");
+        }
+
+    });
+
+}
+
+function getFollows(id){
+
+
+    axios.get('https://api.twitch.tv/helix/users/follows?first=100&from_id=' + id, options).then(response => {
+
+        let add = "";
+
+        response.data.data.map( function (value) {
+
+            add += "&user_id=" + value.to_id; //Generate URL query parameters, in this case a list of users.
+
+        });
+
+        getStreams(add);
+
+    });
+}
+
+function getStreams(add){
+
+    axios.get('https://api.twitch.tv/helix/streams?first=5' + add, options).then(response =>{
+
+
+
+        console.log(response.data);
+        renderStreams(response.data.data.slice(0,5));
+
+
+    })
+
+}
+
+function renderStreams(data){
+    let generatedHTML = '<div class="empty"></div>';
+
+    data.map((stream, i) => {
+
+        generatedHTML += result_template.replace("{NAME}", stream.user_name).replace("{NAME}", stream.user_name).replace("{TITLE}", stream.title).replace("{VIEWERS}", stream.viewer_count.toLocaleString()).replace("{THUMBNAIL}", stream.thumbnail_url.replace("{width}x{height}", "300x168")).replace("{INDEX}", i + 1);
+
+        
+
+    })
+
+    document.getElementById("content_results").innerHTML += generatedHTML + '<div class="empty"></div>';
+}
+
+
+
 function init(){
-
-
+    console.log(options)
+    
+    
     //Update both search bars when value is changed. Needs to be optimized/rewritten.
     let topsearch = document.getElementById("topsearch");
     let midsearch = document.getElementById("midsearch");
@@ -65,13 +154,13 @@ function init(){
         midsearch.value = searchParams.get("id");
         topsearch.value = searchParams.get("id");
         document.getElementById("nav_results").innerHTML = searchParams.get("id").toUpperCase();
-
+        getID(searchParams.get("id"));
     }
     
 }
 
 function search(event){
-
+    location.hash = "#Results"
     location.search = "?id=" + document.getElementById("topsearch").value;
 
 }
